4cdf2ea71d27ac212b7064f378aff5f72644b8b2
fish <pekphet@126.com>
2018-05-30 16:51:54 +0800

    delete installed pkg

--- app/src/main/java/com/xiaozi/appstore/receiver/AppInstallRcv.kt
+++ app/src/main/java/com/xiaozi/appstore/receiver/AppInstallRcv.kt
@@ -3,6 +3,7 @@ package com.xiaozi.appstore.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.fish.fishdownloader.service.deleteInfo
 import com.xiaozi.appstore.component.Framework
 import com.xiaozi.appstore.component.GlobalData
 import com.xiaozi.appstore.manager.NetManager
@@ -18,6 +19,7 @@ class AppInstallRcv : BroadcastReceiver(){
             if (appPackageName != null) {
                 Framework.Package.addInstalled(appPackageName)
                 OBManager.INSTALL_CALLBACK_OBB.notifyObs(appPackageName)
+                deleteInfo(context!!, appPackageName)
                 NetManager.callInstalledUpload(appPackageName)
                 NetManager.fastCall<String>(GlobalData.getCalls(appPackageName)?.install)
             }
